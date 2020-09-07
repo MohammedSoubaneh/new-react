8c6a0474ca26170ea49d8c69146936c3cae1868b
Mohammed Soubaneh <SoubCompany@gmail.com>
2020-09-06 23:34:57 -0400

    push1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <div className="header">
             <div className="innerHeader">
                 <div> <img src="./slackimages\itemerie_1024x1024.png" width="35px"/> </div>
-                <div>Cart</div>
+                <div className="cartHeader">Cart</div>
             </div>
         </div>
     )
