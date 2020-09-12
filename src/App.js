15329b7b50a539686143ca907cf539a7bb5c87ca
Mohammed Soubaneh <SoubCompany@gmail.com>
2020-09-12 00:31:37 -0400

    hero-component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './style.css';
 import Header from './components/Header/Header';
+import SectionOne from './components/Hero/sectionOne';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SectionOne />
     </div>
   );
 }
--- src/components/Hero/sectionOne.js
+++ src/components/Hero/sectionOne.js
@@ -3,7 +3,17 @@ import React from 'react'
 function SectionOne() {
     return (
         <div>
-            <section>One</section>
+            <section className="heroSectionOne">
+                <div><img src="./slackimages\itemerie_1024x1024.png" width="35px"/></div>
+                <div className="title">Find real ecologically responsible items for your home and for you. </div>
+                <div>Discover Our Items</div>
+                <div className="heroSectionTwo">
+                <div className="SubTitleText"> 
+                <h1>What do you mean by real?</h1>
+                <p>These days there are many companies touting organic, vegan, sustainable items. It’s a hot topic on the minds of most people. Unfortunately, it’s also possible to say pretty much anything online and no one will be the wiser as to whether it’s true or not.</p>
+                </div>
+                </div>
+            </section>
         </div>
     )
 }
