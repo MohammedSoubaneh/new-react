e5b23adc6c47a98d7c4ac420b52c3829c9057e6b
Musab Hassan <musab.nassah@gmail.com>
2020-09-15 02:22:14 -0400

    Musab Branch

--- src/components/Hero/sectionOne.js
+++ src/components/Hero/sectionOne.js
@@ -8,11 +8,11 @@ function SectionOne() {
                 <div className="title">Find real ecologically responsible items for your home and for you. </div>
                 <div className="discoverButton">Discover Our Items</div>
                 <div className="heroSectionTwo">
-                <div className="SubTitleText"> 
-                <h1>What do you mean by real?</h1>
-                <p>These days there are many companies touting organic, vegan, sustainable items. It’s a hot topic on the minds of most people. Unfortunately, it’s also possible to say pretty much anything online and no one will be the wiser as to whether it’s true or not.</p>
+                <div className="subTitleText"> What do you mean by real?</div>
+                <div className="subPgText">These days there are many companies touting organic, vegan, sustainable items. It’s a hot topic on the minds of most people. Unfortunately, it’s also possible to say pretty much anything online and no one will be the wiser as to whether it’s true or not.</div>
+                
                 </div>
-                </div>
+                <div className="blueContainer"></div>
             </section>
         </div>
     )
