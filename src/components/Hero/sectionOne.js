0b2bc6cf561046fb4553e680bcb9fd5b898af4ac
Mohammed Soubaneh <SoubCompany@gmail.com>
2020-09-18 00:10:35 -0400

    new-push-now

--- src/components/Hero/sectionOne.js
+++ src/components/Hero/sectionOne.js
@@ -7,12 +7,11 @@ function SectionOne() {
                 <div className="flowerImage" ><img src="./slackimages\itemerie_1024x1024.png" width="50px" /></div>
                 <div className="title">Find real ecologically responsible items for your home and for you. </div>
                 <div className="discoverButton">Discover Our Items</div>
-                <div className="heroSectionTwo">
-                <div className="SubTitleText"> 
-                <h1>What do you mean by real?</h1>
-                <p>These days there are many companies touting organic, vegan, sustainable items. It’s a hot topic on the minds of most people. Unfortunately, it’s also possible to say pretty much anything online and no one will be the wiser as to whether it’s true or not.</p>
+                <div className="heroSectionTwo"> 
+                <div className="subTitleText">What do you mean by real?</div>
+                <div className="subPgText">These days there are many companies touting organic, vegan, sustainable items. It’s a hot topic on the minds of most people. Unfortunately, it’s also possible to say pretty much anything online and no one will be the wiser as to whether it’s true or not.</div> 
                 </div>
-                </div>
+                <div className="blueContainer"></div>
             </section>
         </div>
     )
